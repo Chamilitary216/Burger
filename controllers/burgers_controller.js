81289dce6cfe61a0682bfd8db8d542cf776e35c2
Chris Curry <Chamilitary216@hotmail.com>
2021-03-02 23:42:54 -0500

    created index handlebar html layout

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,28 @@
-//Creates a new burger
+const express = require("express");
+const router = express.Router();
 
-const { Router } = require("express");
+const burger = require ("../models/burger.js");
 
-router.get('/index', function (req, res){
-    burger.selectAll(function (data) {
-        const hbsObject = { burgers: data };
-        res.render('index', hbsObject);
-    })
-})
 
-router.post('/burger/create')
+//Creates routes
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+
+//Creates new burger  POST!
+router.post('/api/create', function (req, res) {
+    burger.create ([
+        "burger_name", "ate"
+    ], [
+        req.body.burger_name, req.body.ate
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
---
+++ server.js
@@ -0,0 +1,33 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(bodyParser.json());
+
+//Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes 
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+//App is listening on port
+
+
+app.listen(PORT, function(){
+    console.log ("Server now listening on port" + PORT);
+})
